fe8db162859df7c47ca74153def792cb37a14a5f
wudan <wudan@shinezone.com>
2017-01-11 21:48:00 +0800

    + tushare

--- tests/test_timeutils.py
+++ tests/test_timeutils.py
@@ -1,5 +1,7 @@
 import pytest
+import unittest
 from easyutils.timeutils import is_holiday
+
 
 def test_is_holiday():
     cases = [('20161001', True), ('20161008', False), ('20161118', False), ('20161119', True)]
